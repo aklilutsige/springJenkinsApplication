b1b07391a275f8dbc10804cc75d65087f5700c68
Aklilu Tsige <aklilutsige@gmail.com>
2021-12-31 15:15:16 +0100

    Add Member model and getAllMember controller

--- src/main/java/et/aklilu/tsige/springjenkinsapplication/controller/HelloController.java
+++ src/main/java/et/aklilu/tsige/springjenkinsapplication/controller/HelloController.java
@@ -1,12 +1,29 @@
 package et.aklilu.tsige.springjenkinsapplication.controller;
 
+import et.aklilu.tsige.springjenkinsapplication.model.Member;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String index(){
-        return "Greetings from spring boot";
+    public List<Member> getAllMembers(){
+        /**
+         * Test Data
+         */
+        Member aklilu = new Member("Aklilu", "Tsige", "30/03/1986", "M");
+        Member tegest = new Member("Tegest", "Tsige", "30/03/1994", "F");
+        Member tilahune = new Member("Thilahune", "Tsige", "24/03/1981", "M");
+        Member enat = new Member("Enat", "Tsige", "28/03/1986", "F");
+        Member neba = new Member("Neba", "Tsige", "15/03/196", "M");
+        Member nebaTg = new Member("Tegest", "Tsige", "30/03/1986", "F");
+
+        List<Member> memberList = new ArrayList<>();
+        memberList.addAll(List.of(aklilu,tegest, tilahune,enat,neba,nebaTg));
+
+        return memberList;
     }
 }
---
+++ src/main/java/et/aklilu/tsige/springjenkinsapplication/model/Member.java
@@ -0,0 +1,47 @@
+package et.aklilu.tsige.springjenkinsapplication.model;
+
+public class Member {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+
+    public Member(String firstName, String lastName, String dateOfBirth, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
